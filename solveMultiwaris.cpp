e9c8540f59990c4deb579789e6d01472446f2d12
rismamutia <rismamutiadewi89@gmail.com>
2025-06-03 18:41:16 +0700

    membuat program utama dengan menggunakan konsep pewarisan

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -34,9 +34,15 @@ class budi : public pekerja, public pelajar {
 public:
     budi(int pUmur) :
         pekerja(pUmur),
-        pelajar(pUmum)
+        pelajar(pUmur)
     {
         cout << "budi dibuat\n" << endl;
     }
 };
+
+int main(){
+    budi a(12);
+    return 0;
+}
+
 
