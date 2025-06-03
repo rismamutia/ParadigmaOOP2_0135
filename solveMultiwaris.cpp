38cfdf24a799a6752d6bcd54ed8190080da06aa2
rismamutia <rismamutiadewi89@gmail.com>
2025-06-03 18:39:59 +0700

    membuat class turunan

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -12,3 +12,31 @@ public:
     }
 };
 
+class pekerja : public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmum)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
+
