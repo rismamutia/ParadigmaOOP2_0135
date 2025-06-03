860c59c16a4cc383008701fdaed697d75b9eca2b
rismamutia <rismamutiadewi89@gmail.com>
2025-06-03 17:24:04 +0700

    membuat class turunan dari class seseorang

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -8,3 +8,10 @@ class seseorang {
     // cout<<"Pesan dari seseorang" << endl;
     //}
 };
+
+class joko :public seseorang {
+    public:
+    void pesan(){
+        cout << "Pesan dari joko" << endl;
+    }
+};
